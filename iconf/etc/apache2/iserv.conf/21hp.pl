97cc642fc9ffcf918732f8a074821fe33c424e61
Felix Jacobi <felix.jacobi@stsbl.de>
2017-05-24 23:29:14 +0200

    Added support for allowing authentificated access from outside of the LAN.

--- iconf/etc/apache2/iserv.conf/21hp.pl
+++ iconf/etc/apache2/iserv.conf/21hp.pl
@@ -9,7 +9,10 @@ my @users = IServ::DB::SelectCol("SELECT act FROM users_priv WHERE privilege = '
 my @groups = IServ::DB::SelectCol("SELECT act FROM groups WHERE deleted IS NULL AND act IN (SELECT act FROM groups_flag WHERE flag = 'hp_from_inet')");
 
 my $UserHomepages = $conf->{UserHomepages};
-
+my $UserHomepagesLAN = $conf->{UserHomepagesLAN};
+my $HomepageExternalAuthAccess = $conf->{HomepageExternalAuthAccess};
+my $Servername = $conf->{Servername};
+my @LAN = @{$conf->{LAN}};
 
 sub group_config() {
   print "  # Group Homepages which are always allowed via hp_from_inet flag.\n";
@@ -33,5 +36,55 @@ sub user_config() {
   }
 }
 
+sub external_auth() {
+  print "  # Add IServ sessauth\n";
+  print "  AddExternalAuth iserv /usr/bin/iservsessauth\n";
+  print "  SetExternalAuthMethod iserv pipe\n";
+  print "\n";
+  print "  # Allow external access to user and group homepages with HTTP Basic Login.\n";
+  print "  <Directory /home/*/Homepage/>\n";
+  print "    Order allow,deny\n";
+  print "    Allow from all\n";
+  print "    AuthType Basic\n";
+  print "    AuthName \"Please sign-in with your IServ account to access this homepage\"\n";
+  print "    AuthBasicProvider external\n";
+  print "    AuthExternal iserv\n";
+  print "    # Require LAN ip or login\n";
+  print "    <RequireAny>\n";
+  print "      Require valid-user\n";
+  print "      # Allow access to always allowed user homepages\n";
+  print "      Require expr %{HTTP_HOST} =~ /(".join("|", @users).").$Servername/\n";
+  print "      # Allow access from LAN\n";
+  foreach my $iprange (@LAN)
+  {
+    print "      Require ip $iprange\n";
+  }
+  print "    </RequireAny>\n";
+  print "  </Directory>\n";
+  print "\n";
+  print "  <Directory /group/*/Homepage/>\n";
+  print "    Order allow,deny\n";
+  print "    Allow from all\n";
+  print "    AuthType Basic\n";
+  print "    AuthName \"Please sign-in with your IServ account to access this homepage\"\n";
+  print "    AuthBasicProvider external\n";
+  print "    AuthExternal iserv\n";
+  print "    # Require LAN ip or login\n";
+  print "    <RequireAny>\n";
+  print "      Require valid-user\n";
+  print "      # Allow access to always allowed group homepages\n";
+  print "      Require expr %{HTTP_HOST} =~ /(".join("|", @groups).").$Servername/\n";
+  print "      # Allow access from LAN\n";
+  foreach my $iprange (@LAN)
+  {
+    print "      Require ip $iprange\n";
+  }
+  print "    </RequireAny>\n";
+  print "  </Directory>\n";
+  print "\n";
+}
+
 group_config() unless @groups < 1 or $UserHomepages == 0;
 user_config() unless @users < 1 or $UserHomepages == 0;
+external_auth() if $HomepageExternalAuthAccess and $UserHomepages
+  and $UserHomepagesLAN;
