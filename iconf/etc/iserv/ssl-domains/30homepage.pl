715db19fed323938dbe16585e685f6ecf27f5f73
Felix Jacobi <felix.jacobi@stsbl.de>
2017-07-11 03:43:32 +0200

    Added iconf/iservchk scripts for ssl cert support.

--- iconf/etc/iserv/ssl-domains/30homepage.pl
+++ iconf/etc/iserv/ssl-domains/30homepage.pl
@@ -15,9 +15,27 @@ print "# list of user and group homepages to include\n";
 my @x = split /\./, hostname;
 my $hostname_s = shift @x;
 my $domain = join '.', @x;
+
+my $sql;
+my @sql_params;
+my $val = IServ::DB::SelectVal "SELECT invert FROM group_flag_def WHERE flag = ?", "hp_ssl_cert";
+
+# flag does not support invert
+if ($val eq 1)
+{
+  #warn "inverting is not supported for flag hp_ssl_cert.\n";
+  $sql = "SELECT act FROM users_priv WHERE privilege = ? AND act != ?";
+  @sql_params = ("hp_ssl_cert", "admins");
+} else
+{
+  $sql = "SELECT act FROM users_priv WHERE privilege = ? AND act != ?".
+     "UNION SELECT act FROM group_flag_assign WHERE flag = ? AND act != ?";
+  @sql_params = ("hp_ssl_cert", "admins", "hp_ssl_cert", "admins");
+}
+
 # admins is covered by default
-my @label = IServ::DB::SelectCol "SELECT act FROM users_priv WHERE privilege = ? AND act != ?".
-  "UNION SELECT act FROM group_flag_assign WHERE flag = ? AND act != ?", "hp_ssl_cert", "admins", "hp_ssl_cert", "admins";
+my @label = IServ::DB::SelectCol $sql, @sql_params;
+
 my @domain = ($domain, @{$conf->{AliasDomains} // []});
 my @fqdn = uniq(map { my $d = $_; map { ($_ ? $_ . '.' : '') . $d } @label }
     @domain);
